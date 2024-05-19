0cec1710b59f635213e5e7f6c03f256d34d70448
tamahhamat <newman.tamah@gmail.com>
2024-05-19 02:47:03 +0200

    homepage basic and styling

--- rafiki-dashboard/src/App.js
+++ rafiki-dashboard/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import ProjectTimeline from './ProjectTimeline';
 
-function App() {
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Rafiki Village Project</h1>
       </header>
+      <div className="App-body">
+        <ProjectTimeline />
+      </div>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/RafikiData" element={<Home />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
